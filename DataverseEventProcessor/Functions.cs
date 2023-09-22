c5684c2d000cbc2c23f3c1fd40f4b9e780f9e5e0
Marius Wodtke <marius.wodtke@gmail.com>
2023-09-22 20:24:33 +0200

    Add a working DataverseEventProcessor as WebJob

---
+++ DataverseEventProcessor/Functions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Xrm.Sdk;
+
+namespace WebJobProcessor
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([ServiceBusTrigger("dataverse", "account-export", Connection = "AzureServiceBus")] RemoteExecutionContext message, ILogger log)
+        {
+            log.LogInformation(message.MessageName);
+        }
+    }
+}
---
+++ DataverseEventProcessor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var hostBuilder = new HostBuilder();
+hostBuilder.ConfigureWebJobs((context, builder) => {
+    builder.AddServiceBus(options => options.MaxConcurrentCalls = 1);
+});
+hostBuilder.ConfigureLogging((context, b) =>
+{
+    b.AddConsole();
+});
+var host = hostBuilder.Build();
+using (host)
+{
+    await host.RunAsync();
+}

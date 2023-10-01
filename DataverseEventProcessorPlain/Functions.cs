bb5d1de7fbf87e680b8323745e3dd7f238702f18
Marius Wodtke <marius.wodtke@gmail.com>
2023-10-01 16:30:12 +0200

    Add a plain console listener that skips the webjobs sdk.

---
+++ DataverseEventProcessorPlain/Functions.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Logging;
+using Microsoft.Xrm.Sdk;
+using System.Runtime.Serialization.Json;
+
+namespace DataverseEventProcessorPlain
+{
+    public class Functions
+    {
+        private ILogger log;
+
+        public Functions()
+        {
+            log = LoggerFactory.Create(_ => _.AddConsole()).CreateLogger("User");
+        }
+
+        public Task ProcessQueueMessage(ProcessMessageEventArgs args)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(RemoteExecutionContext));
+            var message = serializer.ReadObject(args.Message.Body.ToStream()) as RemoteExecutionContext;
+
+            log.LogInformation(message?.MessageName);
+
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessError(ProcessErrorEventArgs args)
+        {
+            log.LogError(args.Exception.Message);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DataverseEventProcessorPlain/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using DataverseEventProcessorPlain;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Starting");
+
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var sbNamespace = config.GetSection("AzureServiceBus")["fullyQualifiedNamespace"];
+//var sbConnectionString = config.GetSection("AzureServiceBus")["connectionString"];
+
+var client = new ServiceBusClient(sbNamespace,new DefaultAzureCredential(), new ServiceBusClientOptions());
+//var client = new ServiceBusClient(sbConnectionString);
+
+var functions = new Functions();
+
+var processor = client.CreateProcessor("dataverse", "account-export", new ServiceBusProcessorOptions());
+processor.ProcessMessageAsync += functions.ProcessQueueMessage;
+processor.ProcessErrorAsync += functions.ProcessError;
+
+await processor.StartProcessingAsync();
+Console.WriteLine("Processing");
+
+Console.ReadKey();
